5e2e29cce2d1f4afd3f1be5c9d6f0d60751df6d1
l2002 <l2002>
2025-10-20 12:44:23 +0700

    fix string API_ROOT

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_ROOT = 'https://trello-api-544t.onrender.com';
+export const API_ROOT = 'https://trello-api-544t.onrender.com/';
 // export const API_ROOT = 'http://localhost:3000/';
